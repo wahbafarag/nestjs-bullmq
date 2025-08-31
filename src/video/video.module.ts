dcb6b268588b908379462adb73a15f4dea6ecf18
Wahby_Farag <wahby.farag@leannode.com>
2025-08-31 15:01:16 +0300

    adding event listenrs , trying job failure too

--- src/video/video.module.ts
+++ src/video/video.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { VideoController } from './video.controller';
 import { BullModule } from '@nestjs/bullmq';
+import { VideoWorker } from './video.worker';
 
 @Module({
   imports: [
@@ -21,6 +22,9 @@ import { BullModule } from '@nestjs/bullmq';
       },
       defaultJobOptions: {
         attempts: 3,
+        removeOnComplete: 1000,
+        removeOnFail: 3000,
+        backoff: 2000, // after failure, wait 2 seconds before retrying
       },
     }),
 
@@ -30,5 +34,6 @@ import { BullModule } from '@nestjs/bullmq';
   ],
 
   controllers: [VideoController],
+  providers: [VideoWorker],
 })
 export class VideoModule {}
--- src/video/video.worker.ts
+++ src/video/video.worker.ts
@@ -1,7 +1,34 @@
-import { Processor, WorkerHost } from '@nestjs/bullmq';
+import { OnWorkerEvent, Processor, WorkerHost } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
 
-@Processor('my-bull-queue')
+@Processor('my-bull-queue', {
+  concurrency: 2, // number of concurrent jobs to process
+}) // queue name
 export class VideoWorker extends WorkerHost {
-  async process(job: Job) {}
+  async process(job: Job) {
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(true);
+      }, 5000);
+    });
+    throw new Error('Simulated job failure');
+  }
+
+  // adding event listeners for worker events
+  @OnWorkerEvent('active') // when job starts processing
+  onActive(job: Job) {
+    console.log('Processing job:', job.id);
+    console.log(`Job ${job.id} is now active; data:`, job.data);
+  }
+
+  @OnWorkerEvent('completed') // when job is completed
+  onCompleted(job: Job) {
+    console.log(`Job ${job.id} has completed`);
+  }
+
+  @OnWorkerEvent('failed') // when job fails
+  onFailed(job: Job, err: Error) {
+    console.log(`Job ${job.id} has failed with error: ${err.message}`);
+    console.log(`Attempts made: ${job.attemptsMade}`);
+  }
 }
